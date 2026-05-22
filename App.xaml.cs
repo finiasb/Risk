373ab5a5b20cf06ed7802da2dacab5ad09251c82
Fineas <finiasbura@gmail.com>
2026-05-22 23:56:05 +0300

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Risk
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Risk
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+            this.MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
+
+            // Acum aplicăm modul full-screen în ordinea corectă
+            this.WindowStyle = WindowStyle.None;
+            this.WindowState = WindowState.Maximized;
+            this.ResizeMode = ResizeMode.NoResize;
+        }
+        private void Provincie_Click(object sender, RoutedEventArgs e)
+        {
+            if (e.OriginalSource is Path hartiutaLovit)
+            {
+                string numeTara = hartiutaLovit.Tag?.ToString() ?? hartiutaLovit.Name;
+
+                MessageBox.Show("Ai apasat pe: " + numeTara);
+            }
+        }
+    }
+}
